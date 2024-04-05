98d8907243260f9e39e81c7f21fe9f621c7513a4
tarikmujcic <TarikMujcic@outlook.com>
2024-04-06 00:13:51 +0200

    Added a test for running just the screenshot processing.

--- fsp/src/test/java/org/example/service/ScreenshotServiceTest.java
+++ fsp/src/test/java/org/example/service/ScreenshotServiceTest.java
@@ -6,7 +6,14 @@ public class ScreenshotServiceTest extends TestCase {
 
     private static final String SCREENSHOT_PATH = "D:\\Desktop\\ImagesBefore";
 
+    private static final String SOURCE_DIRECTORY_PATH = "D:\\Desktop\\ImagesBefore";
+    private static final String TARGET_DIRECTORY_PATH = "D:\\Desktop\\ImagesAfter";
+
     public void testTakeScreenshot() {
         ScreenshotService.takeScreenshot(SCREENSHOT_PATH);
+    }
+
+    public void testProcessScreenshot() {
+        ScreenshotService.processScreenshot(SOURCE_DIRECTORY_PATH, TARGET_DIRECTORY_PATH);
     }
 }
